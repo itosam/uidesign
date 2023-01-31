9793ed5d4b0e78b5a95444c32569460f3e009db2
Osamu Ito <mr.samito@gmail.com>
2023-01-30 21:36:22 -0500

    added outline for homepage and services

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+      <div className="home">
+        <h1>U&I</h1>
+        <h2>A design and development studio based in Philadelphia. </h2>
+        <h6></h6>
+      </div>
+    );
+}
+
+export default Home
---
+++ src/components/Services.jsx
@@ -0,0 +1,20 @@
+const Services = () => {
+    return(
+        <div>
+            <h6>Services</h6>
+        <li>
+            <ul>Brand & Identity</ul>
+            <ul>Web Design & Development</ul>
+            <ul>Brand Strategy</ul>
+            <ul>Interaction Design</ul>
+            <ul>UI/UX</ul>
+            <ul>Product Design</ul>
+        </li>
+
+        <h6>Contact</h6>
+        <p>hello@u-i.digital</p>
+        </div>
+    )
+}
+
+export default Services
