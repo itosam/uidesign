47e23239cb52420c9216064f3afb1b34e5a212e1
Osamu Ito <mr.samito@gmail.com>
2023-01-30 22:12:05 -0500

    added line segment

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,7 @@
 const Services = () => {
     return(
         <div>
-            <h6>Services</h6>
+            <h6>Services</h6>  
         <li>
             <ul>Brand & Identity</ul>
             <ul>Web Design & Development</ul>
@@ -10,7 +10,7 @@ const Services = () => {
             <ul>UI/UX</ul>
             <ul>Product Design</ul>
         </li>
-
+      
         <h6>Contact</h6>
         <p>hello@u-i.digital</p>
         </div>
