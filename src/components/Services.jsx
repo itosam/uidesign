b1e77852c92cda9db2e901e6db22c9b7a0a16638
Osamu Ito <mr.samito@gmail.com>
2023-06-23 16:45:16 -0400

    updating layout

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,36 +1,23 @@
 const Services = () => {
     return (
       <div className="Sevices">
-        <div className="Column">
-        <ul className="Column-heading"><h6>Services</h6></ul>
-        <ul>
+        <row>
+        <column>
+          <h6 className="heading">Services</h6>
           <h6>Brand & Identity</h6>
-        </ul>
-        <ul>
           <h6>Web Design & Development</h6>
-        </ul>
-        <ul>
           <h6>Brand Strategy</h6>
-        </ul>
-        </div>
-        <div className="Column">
-        <br/>
-        <ul>  
+        </column>
+        <column>
           <h6>Interaction Design</h6>
-        </ul>
-        <ul>
           <h6>UI/UX</h6>
-        </ul>
-        <ul>
           <h6>Product Design</h6>
-        </ul>
-        </div>
-        <div className="Column2">
-        <ul className="Column-heading"><h6>Contact</h6></ul>
-        <ul>
-          <h6>hello@u-i.digital</h6>
-        </ul>
-        </div>
+        </column>
+        <column>
+        <h6 className='heading'>Contact</h6>
+        <h6>hello@u-i.digital</h6>
+        </column>
+        </row>
       </div>
     );
 }
