195b818e9faa13ba3a797da2a4d1db14eb7bccf3
Osamu Ito <mr.samito@gmail.com>
2023-07-13 10:59:52 -0400

    updating css responsiveness

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,21 +2,23 @@ const Services = () => {
     return (
       <div className="Sevices">
         <row>
-        <column>
-          <h6 className="heading">Services</h6>
-          <h6>Brand & Identity</h6>
-          <h6>Web Design & Development</h6>
-          <h6>Brand Strategy</h6>
-        </column>
-        <column>
-          <h6>Interaction Design</h6>
-          <h6>UI/UX</h6>
-          <h6>Product Design</h6>
-        </column>
-        <column>
-        <h6 className='heading'>Contact</h6>
-        <h6>hello@u-i.digital</h6>
-        </column>
+          <column>
+            <h6 className="heading">Services</h6>
+            <h6>Brand & Identity</h6>
+            <h6>Web Design & Development</h6>
+            <h6>Brand Strategy</h6>
+          </column>
+          <column>
+            <h6 style={{paddingTop:"70px"}}>
+              Interaction Design
+            </h6>
+            <h6>UI/UX</h6>
+            <h6>Product Design</h6>
+          </column>
+          <column>
+            <h6 className="heading">Contact</h6>
+            <h6>hello@u-i.digital</h6>
+          </column>
         </row>
       </div>
     );
